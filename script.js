56143e959b7fd9ed9d98cab84aaca47ec662612f
Parlgy <parlg4776@gmail.com>
2023-10-11 15:35:05 +0300

    updated services carousel

--- script.js
+++ script.js
@@ -1,88 +1,26 @@
-const toggleButton1 = document.getElementById('toggleButton1');
-const collapsibleContent = document.getElementById('collapsibleContent');
 
-toggleButton1.addEventListener('click', () => {
-    if (collapsibleContent.style.display === 'none' || collapsibleContent.style.display === '') {
-        collapsibleContent.style.display = 'block';
-    } else {
-        collapsibleContent.style.display = 'none';
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
     }
-});
-
-const toggleButton2 = document.getElementById('toggleButton2');
-const collapsibleContent2 = document.getElementById('collapsibleContent2');
-
-toggleButton2.addEventListener('click', () => {
-    if (collapsibleContent2.style.display === 'none' || collapsibleContent2.style.display === '') {
-        collapsibleContent2.style.display = 'block';
-    } else {
-        collapsibleContent2.style.display = 'none';
-    }
-});
-
-const toggleButton3 = document.getElementById('toggleButton3');
-const collapsibleContent3 = document.getElementById('collapsibleContent3');
-
-toggleButton3.addEventListener('click', () => {
-    if (collapsibleContent3.style.display === 'none' || collapsibleContent3.style.display === '') {
-        collapsibleContent3.style.display = 'block';
-    } else {
-        collapsibleContent3.style.display = 'none';
-    }
-});
+})
 
 
-const toggleButton4 = document.getElementById('toggleButton4');
-const collapsibleContent4 = document.getElementById('collapsibleContent4');
 
-toggleButton4.addEventListener('click', () => {
-    if (collapsibleContent4.style.display === 'none' || collapsibleContent4.style.display === '') {
-        collapsibleContent4.style.display = 'block';
-    } else {
-        collapsibleContent4.style.display = 'none';
-    }
-});
 
-const toggleButton5 = document.getElementById('toggleButton5');
-const collapsibleContent5 = document.getElementById('collapsibleContent5');
-
-toggleButton5.addEventListener('click', () => {
-    if (collapsibleContent5.style.display === 'none' || collapsibleContent5.style.display === '') {
-        collapsibleContent5.style.display = 'block';
-    } else {
-        collapsibleContent5.style.display = 'none';
-    }
-});
-
-const toggleButton6 = document.getElementById('toggleButton6');
-const collapsibleContent6 = document.getElementById('collapsibleContent6');
-
-toggleButton6.addEventListener('click', () => {
-    if (collapsibleContent6.style.display === 'none' || collapsibleContent6.style.display === '') {
-        collapsibleContent6.style.display = 'block';
-    } else {
-        collapsibleContent6.style.display = 'none';
-    }
-});
-
-const toggleButton7 = document.getElementById('toggleButton7');
-const collapsibleContent7 = document.getElementById('collapsibleContent7');
-
-toggleButton7.addEventListener('click', () => {
-    if (collapsibleContent7.style.display === 'none' || collapsibleContent7.style.display === '') {
-        collapsibleContent7.style.display = 'block';
-    } else {
-        collapsibleContent7.style.display = 'none';
-    }
-});
-
-const toggleButton8 = document.getElementById('toggleButton8');
-const collapsibleContent8 = document.getElementById('collapsibleContent8');
-
-toggleButton8.addEventListener('click', () => {
-    if (collapsibleContent8.style.display === 'none' || collapsibleContent8.style.display === '') {
-        collapsibleContent8.style.display = 'block';
-    } else {
-        collapsibleContent8.style.display = 'none';
-    }
-});
