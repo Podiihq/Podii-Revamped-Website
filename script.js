1f1346ea0adf4c928c85d5277fbd7a4bd1f8b98d
Parlgy <parlg4776@gmail.com>
2024-02-26 15:33:33 +0300

    Added countdown

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:30,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    dots:true,
-    responsive:{
-        0:{
-            items:1
+    loop: true,
+    margin: 30,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:2
+        600: {
+            items: 2
         },
-        1000:{
-            items:3
+        1000: {
+            items: 3
         }
     }
 })
+
 
 
 
