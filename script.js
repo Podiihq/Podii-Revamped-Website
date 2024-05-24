938bbc3432bfca93dc5d3f30196f6427a7d00158
Parlgy <parlg4776@gmail.com>
2024-05-24 09:21:58 +0300

    update

--- script.js
+++ script.js
@@ -26,49 +26,6 @@ $('.owl-carousel').owlCarousel({
 
 // Hero Marquee
 
-// const off = 10;
-// let l = off;
-// const marqueeElements = Array.from(document.querySelectorAll('.marquee a'));
-// const speed = 0.5;
-// const stack = [];
-// let pause = false;
-
-// marqueeElements.forEach(element => {
-//     const width = element.offsetWidth + off;
-//     element.style.left = `${l}px`;
-//     l += width;
-//     stack.push(element);
-// });
-
-// function moveMarquee() {
-//     if (!pause) {
-//         marqueeElements.forEach(element => {
-//             const currentLeft = parseFloat(getComputedStyle(element).left);
-//             const newLeft = currentLeft - speed;
-//             element.style.left = `${newLeft}px`;
-
-//             if (newLeft + element.offsetWidth < -130) {
-//                 const firstElement = stack.shift();
-//                 const lastElement = stack[stack.length - 1];
-//                 element.style.left = `${parseFloat(getComputedStyle(lastElement).left) + lastElement.offsetWidth + off}px`;
-//                 stack.push(element);
-//             }
-//         });
-//     }
-//     requestAnimationFrame(moveMarquee);
-// }
-
-// requestAnimationFrame(moveMarquee);
-
-// const marqueeContainer = document.querySelector('.marquee');
-// marqueeContainer.addEventListener('mouseover', () => {
-//     pause = true;
-// });
-// marqueeContainer.addEventListener('mouseout', () => {
-//     pause = false;
-// });
-
-
 const off = 10;
 let l = off;
 const marqueeElements = Array.from(document.querySelectorAll('.marquee a'));
