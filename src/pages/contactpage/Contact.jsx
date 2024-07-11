d4931aabc7beb92e0cc07088ad275a99ab33f62d
Parlgy <parlg4776@gmail.com>
2024-07-11 11:48:58 +0300

    Update

--- src/pages/contactpage/Contact.jsx
+++ src/pages/contactpage/Contact.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import emailjs from "@emailjs/browser";
 import Navbar from '../../global-components/NavBar';
 import Lenis from 'lenis';
 import Footer from '../../global-components/Footer';
 import { useNavigate } from 'react-router';
+import gsap from 'gsap';
+import { animateHeroTitle } from '../../global-animations/animation';
 
 const ContactForm = () => {
+    const heroRef = useRef(null);
+    const timeline = useRef(gsap.timeline());
+
     useEffect(() => {
         const lenis = new Lenis();
         lenis.on('scroll', (e) => {
@@ -20,6 +25,17 @@ const ContactForm = () => {
         return () => {
             lenis.destroy();
         };
+    }, []);
+
+    useEffect(() => {
+        const context = gsap.context(() => {
+            const tl = timeline.current;
+
+            tl
+                .add(animateHeroTitle(), "=+10%")
+
+        }, heroRef);
+        return () => context.revert();
     }, []);
 
     const [formData, setFormData] = useState({
@@ -117,19 +133,18 @@ const ContactForm = () => {
 
     return (
         <>
-            <div className='bg-[#F4FBF8]'>
+            <div className='bg-[#F4FBF8]' ref={heroRef}>
                 <Navbar />
-                <section className='lg:pt-20 pt-10'>
+                <section className='pt-10 lg:pt-20'>
                     <div className="px-4 lg:pb-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pt-20 text-[#303030]">
                         <div className="flex items-center justify-center lg:flex-row">
                             <div className="mb-4 lg:max-w-xl lg:pr-5 lg:mb-0">
                                 <div className="max-w-xl mb-6 mt-16 md:mt-4 text-center md:text-left">
-                                    <h2 className="text-center mb-8 text-4xl lg:text-[56px] sm:leading-none text-[#307457]"
-                                    >
+                                    <h2 className="text-center mb-8 text-4xl lg:text-[56px] sm:leading-none text-[#307457]" data-text-animation>
                                         Tell us about your project
                                     </h2>
 
-                                    <p className="text-center text-xl md:text-2xl mont-regular">
+                                    <p className="text-center text-xl md:text-2xl mont-regular" data-text-animation>
                                         We do custom software right and can't wait to work with you. Your project will shine with our expertise.
                                     </p>
                                 </div>
@@ -138,17 +153,17 @@ const ContactForm = () => {
                     </div>
                 </section>
             </div>
-            <section className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-2xl md:px-24 lg:px-8 lg:py-20 mt-16 lg:mt-16">
+            <section className="px-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-2xl md:px-24 lg:px-8 lg:py-20">
                 <div className="space-y-10 divide-y divide-gray-900/10">
                     <div className="flex justify-center">
                         <form className="md:col-span-2 bg-white" id="contact_form" onSubmit={sendMail}>
                             <div className="px-4 py-6 sm:p-8">
                                 <div className="flex flex-col gap-5">
                                     <div className="sm:col-span-8">
-                                        <label htmlFor="userName" className="block text-lg leading-6 text-gray-900">
+                                        <label htmlFor="userName" className="block text-lg leading-6 text-gray-900" data-text-animation>
                                             Your full name? *
                                         </label>
-                                        <div className="mt-2">
+                                        <div className="mt-2" data-text-animation>
                                             <input
                                                 type="text"
                                                 required
@@ -156,7 +171,7 @@ const ContactForm = () => {
                                                 name="userName"
                                                 placeholder="John Doe"
                                                 autoComplete="given-name"
-                                                className="block w-full rounded-md border-0 py-3.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                className="block w-full rounded-md border-0 py-3.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#FF5E0E] sm:text-sm sm:leading-6"
                                                 value={formData.userName}
                                                 onChange={handleChange}
                                             />
@@ -164,7 +179,7 @@ const ContactForm = () => {
                                         {errors.userName && <div className="error-message text-red-500">{errors.userName}</div>}
                                     </div>
 
-                                    <div className="sm:col-span-8">
+                                    <div className="sm:col-span-8" data-text-animation>
                                         <label htmlFor="email" className="block mb-2 text-lg leading-6 text-gray-900">
                                             Your email? *
                                         </label>
@@ -175,15 +190,16 @@ const ContactForm = () => {
                                             id="email"
                                             placeholder="email@email.com"
                                             autoComplete="given-name"
-                                            className="block w-full rounded-md border-0 py-3.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            className="block w-full rounded-md border-0 py-3.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#FF5E0E] sm:text-sm sm:leading-6"
                                             value={formData.email}
                                             onChange={handleChange}
+                                            data-text-animation
                                         />
                                         {errors.email && <div className="error-message text-red-500">{errors.email}</div>}
                                     </div>
 
                                     <div className="sm:col-span-8">
-                                        <label htmlFor="country" className="block mb-2 text-lg leading-6 text-gray-900">
+                                        <label htmlFor="country" className="block mb-2 text-lg leading-6 text-gray-900" data-text-animation>
                                             Your Country? *
                                         </label>
                                         <input
@@ -193,15 +209,16 @@ const ContactForm = () => {
                                             id="country"
                                             placeholder="Kenya"
                                             autoComplete="given-name"
-                                            className="block w-full rounded-md border-0 py-3.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                            className="block w-full rounded-md border-0 py-3.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#FF5E0E] sm:text-sm sm:leading-6"
                                             value={formData.country}
                                             onChange={handleChange}
+                                            data-text-animation
                                         />
                                         {errors.country && <div className="error-message text-red-500">{errors.country}</div>}
                                     </div>
 
                                     <div className="sm:col-span-8">
-                                        <label htmlFor="checkboxGroup" className="block text-lg leading-6 text-gray-900 mb-6">
+                                        <label htmlFor="checkboxGroup" className="block text-lg leading-6 text-gray-900 mb-4" data-text-animation>
                                             What solutions do you need today?*
                                         </label>
                                         {[
@@ -214,7 +231,7 @@ const ContactForm = () => {
                                             'Streamlined Software Delivery (DevOps)',
                                             'Other (Your Unique Software)',
                                         ].map((service, index) => (
-                                            <div className="flex items-center gap-6 mb-3" key={index}>
+                                            <div className="flex items-center gap-3 mb-2" key={index} data-text-animation>
                                                 <input
                                                     type="checkbox"
                                                     id={`checkbox${index + 1}`}
@@ -224,7 +241,7 @@ const ContactForm = () => {
                                                     checked={formData.services.includes(service)}
                                                     onChange={handleChange}
                                                 />
-                                                <label htmlFor={`checkbox${index + 1}`} className="text-base lg:text-xl font-light">
+                                                <label htmlFor={`checkbox${index + 1}`} className="text-base lg:text-xl mont-regular">
                                                     {service}
                                                 </label>
                                             </div>
@@ -233,18 +250,18 @@ const ContactForm = () => {
                                     </div>
 
                                     <div className="col-span-8">
-                                        <label htmlFor="other" className="block text-xl leading-6 text-gray-900">
+                                        <label htmlFor="other" className="block text-xl leading-6 text-gray-900" data-text-animation>
                                             Please describe your projects in a few words?*
                                         </label>
-                                        <span className="italic text-gray-500">(Minimum of 20 words)</span>
-                                        <div className="mt-4">
+                                        <span className="italic text-gray-500" data-text-animation>(Minimum of 20 words)</span>
+                                        <div className="mt-4" data-text-animation>
                                             <textarea
                                                 id="other"
                                                 required
                                                 placeholder="Type something here..."
                                                 name="other"
                                                 rows="5"
-                                                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#FF5E0E]sm:text-sm sm:leading-6"
                                                 value={formData.other}
                                                 onChange={handleChange}
                                             ></textarea>
@@ -253,18 +270,19 @@ const ContactForm = () => {
                                     </div>
 
                                     <div className="col-span-8 relative w-full">
-                                        <label htmlFor="budget" className="mb-4 block text-xl leading-6 text-gray-900">
+                                        <label htmlFor="budget" className="mb-4 block text-xl leading-6 text-gray-900" data-text-animation>
                                             What is your estimated budget for this project?*
                                         </label>
                                         <select
                                             id="budget"
                                             required
                                             name="budget"
-                                            className="px-2 rounded-md text-xl h-10 w-full border-2 border-gray-300 bg-transparent text-gray-900 focus:outline-none focus:border-rose-600"
+                                            className="px-2 rounded-md text-xl h-10 w-full border-2 mont-regular border-gray-300 bg-transparent text-gray-900 focus:outline-none focus:border-[#FF5E0E]"
                                             value={formData.budget}
                                             onChange={handleChange}
+                                            data-text-animation
                                         >
-                                            <option value="" disabled selected className="text-gray-400">
+                                            <option value="" disabled selected className="text-gray-400 mont-regular">
                                                 Select your budget range
                                             </option>
                                             <option value="$20,000 and above">$20,000 and above</option>
