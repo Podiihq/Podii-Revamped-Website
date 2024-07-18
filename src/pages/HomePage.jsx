f2bb1f5e938e8266115690d1d5346b7fb127d73a
Parlgy <parlg4776@gmail.com>
2024-07-18 09:08:20 +0300

    meta tags test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -49,30 +49,30 @@ const HomePage = () => {
     }, []);
     return (
         <div>
-            {/* <Helmet>
+            <Helmet>
                 <title>Custom Software Development Company in Kenya</title>
                 <meta name="description" content="We are a software consulting company extending modern software, mobile, and web application development." />
 
-                
+
                 <meta property="og:url" content="https://podiihq.com/" />
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="Custom Software Development Company in Kenya" />
                 <meta property="og:description" content="Podii is a custom software development and consulting company. Find innovative and affordable tech solutions by our expert software developers." />
                 <meta property="og:image" content="https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80" />
 
-                
+
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta property="twitter:url" content="https://podiihq.com/" />
                 <meta name="twitter:title" content="Custom Software Development Company in Kenya" />
                 <meta name="twitter:description" content="Podii is a custom software development and consulting company. Find innovative and affordable tech solutions by our expert software developers." />
                 <meta name="twitter:image" content="https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80" />
 
-                
+
                 <meta property="og:title" content="Custom Software Development Company in Kenya" />
                 <meta property="og:description" content="Podii is a custom software development and consulting company. Find innovative and affordable tech solutions by our expert software developers." />
                 <meta property="og:image" content="https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80" />
                 <meta property="og:url" content="https://podiihq.com/" />
-            </Helmet> */}
+            </Helmet>
             <div ref={heroRef}>
                 <Navbar />
                 <section className="px-4 pt-20 lg:pt-4 pb-10 container mx-auto lg:px-8 mt-8 text-[#303030]">
